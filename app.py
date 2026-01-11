4b20a2c09f6cf3315cd544298e40c365a5a299ce
Vardhanraiz <167404535+Vardhanraiz@users.noreply.github.com>
2026-01-11 17:25:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
+from flask import Flask, render_template, request
+
+app = Flask(alzheimer_model.h5)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Dummy prediction
+    prediction = "Mild Demented"
+    return render_template(
+        'result.html',
+        prediction=prediction,
+        gradcam_image='gradcam.png'
+    )
+
+if __name__ == '__main__':
+    app.run(debug=True)
 Add Streamlit application
+
